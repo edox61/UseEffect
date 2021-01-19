1528e61dbee101aa61d62d39b19f3924381015a4
edox61 <63751822+edox61@users.noreply.github.com>
2021-01-19 18:27:36 -0300

    sin dependencias

---
+++ src/components/Lifecycle.js
@@ -0,0 +1,20 @@
+import React, { useState, useEffect } from "react";
+
+const Lifecycle = () => {
+  const [contador1, setContador1] = useState(0);
+  const [contador2, setContador2] = useState(0);
+
+  useEffect(() => {
+    console.log("Sindependencias");
+  });
+  return (
+    <div>
+      <button onClick={() => setContador1(contador1 + 1)}>contador1</button>
+      <button onClick={() => setContador2(contador2 + 1)}>contador2</button>
+      <h1>Contador1 {contador1}</h1>
+      <h1>Contador2 {contador2}</h1>
+    </div>
+  );
+};
+
+export default Lifecycle;
